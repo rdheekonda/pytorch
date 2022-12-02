5a49ae27ce63029d90fee6d54cfccfbd75369044
Wu, Chunyuan <chunyuan.wu@intel.com>
2022-12-02 09:08:30 +0800

    Inductor cpp wrapper: support None as output
    ghstack-source-id: f08440a0b231f2545f2eb7a7907f6b703d215c04
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88560

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -1455,6 +1455,9 @@ class ReinterpretView(BaseView):
             return f"as_strided({self.get_name()}, {size}, {stride}, {offset})"
         return f"as_strided({self.get_name()}, {size}, {stride})"
 
+    def cpp_wrapper_codegen_reference(self):
+        return self.codegen_reference()
+
 
 class SliceView(View):
     @classmethod
@@ -1896,6 +1899,9 @@ class Buffer(IRNode):
     def codegen_reference(self):
         return self.get_name()
 
+    def cpp_wrapper_codegen_reference(self):
+        return self.codegen_reference()
+
     def decide_layout(self):
         pass
 
@@ -1950,10 +1956,16 @@ class RandSeedBuffer(ConstantBuffer):
         # the value does not get clobbered by the time backwards is run.
         return self.get_name() + ".clone()"
 
+    def cpp_wrapper_codegen_reference(self):
+        return self.codegen_reference()
+
 
 class NoneAsConstantBuffer(IRNode):
     def codegen_reference(self):
         return "None"
+
+    def cpp_wrapper_codegen_reference(self):
+        return "at::Tensor()"
 
 
 class ShapeAsConstantBuffer(IRNode):
@@ -1963,6 +1975,9 @@ class ShapeAsConstantBuffer(IRNode):
 
     def codegen_reference(self):
         return str(self.shape)
+
+    def cpp_wrapper_codegen_reference(self):
+        return self.codegen_reference()
 
 
 @dataclasses.dataclass
